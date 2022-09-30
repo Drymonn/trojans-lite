0a18371d4fc6487d743e8a6a80bf177181ca08be
Dennis ID <113302394+Drymonn@users.noreply.github.com>
2022-09-30 10:21:57 +0700

    Create DDOS.py
    Tes

---
+++ DDOS.py
@@ -0,0 +1,11 @@
+import time
+import socket
+import random
+import sys
+def usage():
+    print ("##########################################"): 
+    print ("## command python2 DDOS.py ip port packet"):
+    print ("##    author ===>>> Drymonn"):
+    print ("##    Team ===>>> GanosecTeam"):
+    print ("##    Instagram ===>>> Anakjah4t"):
+    print ("##########################################"):
