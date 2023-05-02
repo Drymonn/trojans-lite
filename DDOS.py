9f25a771567ad93e6916ef5c0a425185ce93ece5
DENN5S.ID <113302394+Drymonn@users.noreply.github.com>
2023-05-02 09:03:51 +0700

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -5,9 +5,9 @@ import sys
 def usage():
     print ("##########################################"): 
     print ("## command python2 DDOS.py ip port packet"):
-    print ("##    author ===>>> Drymonn"):
-    print ("##    Team ===>>> GanosecTeam"):
-    print ("##    Instagram ===>>> Anakjah4t"):
+    print ("##    author ===>>> DENNI5.ID"):
+    print ("##    Team ===>>> FourDeathTeam"):
+    print ("##    Instagram ===>>> xynx_jeoll"):
     print ("##########################################"):
 def flood(victim, vport, duration):
     
